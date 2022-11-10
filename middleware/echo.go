8bd003f6f4a709905bf826b228e8685e2747f3d9
BF-Moritz <bf_moritz@yahoo.de>
2022-11-10 13:16:32 +0100

    added echo middleware

--- middleware/echo.go
+++ middleware/echo.go
@@ -10,17 +10,19 @@ func MakeEchoMiddleware(logger *logger.Logger) echo.MiddlewareFunc {
 	return middleware.RequestLoggerWithConfig(
 		middleware.RequestLoggerConfig{
 			LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+
 				if v.Error == nil {
-					logger.LogInfo("echo", "%s | %s in %v", v.Method, v.URIPath, v.Latency)
+					logger.LogInfo("echo", "%d | %s %s <%v>", v.Status, v.Method, v.URI, v.Latency)
 					return nil
 				}
-				logger.LogError("echo", "%s | %s in %s (%s)", v.Method, v.URI, v.Latency, v.Error)
+				logger.LogError("echo", "%d | %s %s <%s> (%s)", v.Status, v.Method, v.URI, v.Latency, v.Error)
 				return v.Error
 			},
 			LogError:   true,
 			LogLatency: true,
 			LogMethod:  true,
-			LogURIPath: true,
+			LogStatus:  true,
+			LogURI:     true,
 		},
 	)
 }
